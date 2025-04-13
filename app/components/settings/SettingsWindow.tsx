4906d4dc34e93dbc14b0388213c0306189ea751b
Rajesh Kumar Pradhan <p.rajeshkumar2603@gmail.com>
2025-04-14 02:27:54 +0530

    v1.2 nexus added on top

--- app/components/settings/SettingsWindow.tsx
+++ app/components/settings/SettingsWindow.tsx
@@ -102,7 +102,7 @@ export const SettingsWindow = ({ open, onClose }: SettingsProps) => {
                     GitHub
                   </a>
                   <a
-                    href="https://titannexus-production.up.railway.app/"
+                    href="https://titannexus-production.up.railway.app/documentation/"
                     target="_blank"
                     rel="noopener noreferrer"
                     className={classNames(styles['settings-button'], 'flex items-center gap-2')}
