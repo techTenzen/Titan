f70799b3b6e90cf1758d0195d145f89933d4ebbc
Rajesh Kumar Pradhan <p.rajeshkumar2603@gmail.com>
2025-04-25 23:22:50 +0530

    v1.4 logo update change

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -16,11 +16,10 @@ export function Header() {
       })}
     >
       <div className="flex items-center gap-2 z-logo text-bolt-elements-textPrimary cursor-pointer">
-        <div className="i-ph:sidebar-simple-duotone text-xl" />
+        <div className="i-ph:sidebar-simple-duotone text-3xl" />
         <a href="/" className="text-2xl font-semibold text-accent flex items-center">
           {/* <span className="i-bolt:logo-text?mask w-[46px] inline-block" /> */}
-          <img src="/generated-image.png" alt="logo" className="w-[90px] inline-block dark:hidden" />
-          <img src="/generated-image.png" alt="logo" className="w-[90px] inline-block hidden dark:block" />
+          <img src="/generated-image.png" alt="logo" className="w-[200px] h-[180px]" />
         </a>
       </div>
       {chat.started && ( // Display ChatDescription and HeaderActionButtons only when the chat has started.
